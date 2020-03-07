fc97c7a60b4a80949e9c2f5aa075f6de3487ee17
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-03-06 23:38:22 -0300

    feat: changed firebase config to env variables

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,14 +4,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: 'AIzaSyDHPP1yRYw_lqMj_z1xBGBGZr6z8018mkE',
-  authDomain: 'school-app-fadf9.firebaseapp.com',
-  databaseURL: 'https://school-app-fadf9.firebaseio.com',
-  projectId: 'school-app-fadf9',
-  storageBucket: 'school-app-fadf9.appspot.com',
-  messagingSenderId: '737176895182',
-  appId: '1:737176895182:web:e0b61bf6b8819784a5da9f',
-  measurementId: 'G-7ND9Y9SETT',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 });
 
 export const firebaseApp = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
